3464ad328f6faa976bbf33509be3dbc68b1a5914
Zain <zaiabdullah99@gmail.com>
2022-06-14 09:49:47 +0700

    (FIX) function parse()

--- example-1/src/TagParser.php
+++ example-1/src/TagParser.php
@@ -14,8 +14,8 @@ class TagParser
          * tanda / awal dan tanda / akhir itu tanda mulai dan akhir
          * tanda koma (,)   : split berdasarkan koma
          * tanda tanya (?)  : karakter sebelumnya itu opsional (boleh ada boleh tidak), contoh disitu karakter sebelumnya adalah spasi
-         * tanda yang dikurung [] : karakter yang dikurung itu harus ada, contoh disitu karakter yang dikurung adalah koma (,) dan pipe (|)
+         * tanda yang dikurung [] : karakter yang dikurung itu harus ada, contoh disitu karakter yang dikurung adalah spasi ( ) dan koma (,) dan pipe (|)
          */
-        return preg_split('/ ?[,|-] ?/', $tags); // preg_split() adalah fungsi yang mengubah string menjadi array
+        return preg_split('/ ?[ ,|-] ?/', $tags); // preg_split() adalah fungsi yang mengubah string menjadi array
     }
 }
